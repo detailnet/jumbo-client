24db11bb0f575098c4af5d0571063a9a1e1e3625
Ivan Wolf <iwolf@detailag.ch>
2015-11-05 17:15:04 +0100

    Added operation to list methods by week

--- src/Denner/Client/ArticlesClient.php
+++ src/Denner/Client/ArticlesClient.php
@@ -13,4 +13,40 @@ use Denner\Client\Response;
  */
 class ArticlesClient extends DennerClient
 {
+    /**
+     * @param integer $year
+     * @param integer $week
+     * @param array $params
+     * @return Response\ListResponse
+     */
+    public function listPromotionsByWeek($year, $week, array $params = array())
+    {
+        $weekFilters = array(
+            'year' => array('property' => 'year', 'value' => $year, 'operator' => '=', 'type' => 'integer'),
+            'week' => array('property' => 'week', 'value' => $week, 'operator' => '=', 'type' => 'integer'),
+        );
+
+        // We may need to replace already existing filters
+        if (isset($params['filter']) && is_array($params['filter'])) {
+            $filters = array();
+
+            foreach ($params['filter'] as $filter) {
+                if (isset($filter['property']) && isset($weekFilters[$filter['property']])) {
+                    $filters[] = $weekFilters[$filter['property']];
+                    unset($weekFilters[$filter['property']]);
+                } else {
+                    $filters[] = $filter;
+                }
+            }
+
+            // Append remaining filters
+            $filters += $weekFilters;
+        } else {
+            $filters = array_values($weekFilters);
+        }
+
+        $params['filter'] = $filters;
+
+        return $this->listPromotions($params);
+    }
 }
