4bd0ddd5a0ff3008a0054cd8888485b4e6337fc9
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2015-11-20 10:31:50 +0100

    Added integer for listing filter types

--- src/Denner/Client/ServiceDescription/Assets.php
+++ src/Denner/Client/ServiceDescription/Assets.php
@@ -68,7 +68,7 @@ return array(
                 ),
                 'value' => array(
                     'description' => 'The value to filter against',
-                    'type' => array('array', 'string'),
+                    'type' => array('array', 'string', 'integer'),
                     'required' => true,
                 ),
                 'operator' => array(
