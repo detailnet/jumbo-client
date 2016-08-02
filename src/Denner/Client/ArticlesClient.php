31c381c7c549a639fbb04edb8df850f066a033c8
Kevin Jüni <kjueni@detailag.ch>
2016-08-02 17:06:45 +0200

    Added new method for listing of advertised articles DENNER-703

--- src/Denner/Client/ArticlesClient.php
+++ src/Denner/Client/ArticlesClient.php
@@ -33,6 +33,25 @@ class ArticlesClient extends DennerClient
     }
 
     /**
+     * @param integer $year
+     * @param integer $week
+     * @param array $params
+     * @return Response\ListResponse
+     */
+    public function listAdvertisedArticlesByWeek($year, $week, array $params = array())
+    {
+        $filters = array(
+            'year' => array('property' => 'year', 'value' => $year, 'operator' => '=', 'type' => 'integer'),
+            'week' => array('property' => 'week', 'value' => $week, 'operator' => '=', 'type' => 'integer'),
+        );
+
+        // We may need to replace an already existing filter
+        $this->addOrReplaceFilters($filters, $params);
+
+        return $this->listAdvertisedArticles($params);
+    }
+
+    /**
      * @param string $promotionId
      * @param array $params
      * @return Response\ListResponse
