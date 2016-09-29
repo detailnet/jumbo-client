4bbe1fdb9ec35786407425b9cd638a30c15ca771
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2016-09-29 14:04:59 +0200

    Implemented appraisal client with listing and fetch of ratings

---
+++ src/Denner/Client/AppraisalClient.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Denner\Client;
+
+use Denner\Client\Response;
+
+/**
+ * Denner Articles Service client.
+ *
+ * @method Response\ListResponse listRatings(array $params = array())
+ * @method Response\ResourceResponse fetchRating(array $params = array())
+ */
+class AppraisalClient extends DennerClient
+{
+}
